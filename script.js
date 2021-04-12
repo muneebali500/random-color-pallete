078a2f4e56ebded61de77654197524b97fa52745
muneebali500 <72123497+muneebali500@users.noreply.github.com>
2021-04-12 18:19:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const refreshBtn = document.getElementById(`refresh`);
+const colorContainer = document.getElementById(`container`);
+
+refreshBtn.addEventListener(`click`, generatePalette);
+
+function generatePalette() {
+  colorContainer.innerHTML = ``;
+
+  for (let i = 0; i < 60; i++) {
+    const chars = `abcdef0123456789`;
+    let color = ``;
+
+    for (let i = 0; i < 6; i++) {
+      const randomNumber = Math.floor(Math.random() * chars.length);
+      color += chars[randomNumber];
+    }
+
+    const colorEl = document.createElement(`div`);
+    colorEl.innerHTML = `<input value="#${color}" readonly/>
+                        <button onClick=copyColor(this)>copy</button>`;
+    colorEl.style.backgroundColor = `#${color}`;
+
+    colorContainer.appendChild(colorEl);
+  }
+}
+
+generatePalette();
+
+function copyColor(btn) {
+  const copyText = btn.previousElementSibling;
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  alert("Copied the text: " + copyText.value);
+}
